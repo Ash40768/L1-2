6adc36d34ad79cb39a7eaef0574cd1ad6aad091c
Ashutosh Mahapatra <94985469+Ash40768@users.noreply.github.com>
2022-05-21 14:37:03 +0530

    L1-2

---
+++ src/App.js
@@ -0,0 +1,15 @@
+
+import "./App.css";
+import MobileApp from "./components/mobileApp";
+
+function App() {
+  return (
+    <div className="App">
+      <div>
+        <MobileApp />
+      </div>
+    </div>
+  );
+}
+
+export default App;
